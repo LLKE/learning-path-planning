e948a445485680a9297dc9d4be403ac7f1432d60
Laurens Keuker <laurens.keuker@gmail.com>
2025-03-07 16:46:55 +0100

    Fixes #5: Added separate animation function file

--- path_planning.py
+++ path_planning.py
@@ -1,56 +1,9 @@
-import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib.animation as animation
 import yaml
 from a_star import a_star
 from theta_star import theta_star
 from hybrid_a_star import hybrid_a_star
-
-def animate_pathfinding(grid, steps, start, goal, path_found):
-    """Animates the pathfinding process with start and goal markers."""
-    fig, ax = plt.subplots()
-    grid = np.array(grid)
-    ax.imshow(grid, cmap="Greys", origin="upper")
-    ax.set_xticks(np.arange(0, grid.shape[1], 1))
-    ax.set_yticks(np.arange(0, grid.shape[0], 1))
-    ax.grid(which="major", color="black", linestyle='-', linewidth=0.5)
-    ax.tick_params(which="both", bottom=False, left=False, labelbottom=False, labelleft=False)
-    
-    def update(frame):
-        ax.clear()
-        ax.imshow(grid, cmap="Greys", origin="upper")
-        ax.set_xticks(np.arange(0, grid.shape[1], 1))
-        ax.set_yticks(np.arange(0, grid.shape[0], 1))
-        ax.grid(which="major", color="black", linestyle='-', linewidth=0.5)
-        ax.tick_params(which="both", bottom=False, left=False, labelbottom=False, labelleft=False)
-        
-        ax.plot(start[1], start[0], "go", markersize=6, label="Start")
-        ax.plot(goal[1], goal[0], "bo", markersize=6, label="Goal")
-        
-        explored, parent = steps[frame]
-        for (x, y) in explored:
-            ax.plot(y, x, "ro", markersize=3)
-        if frame == len(steps) - 1:
-            # Plot the final path in green
-            if goal in parent:
-                node = goal
-                while node != start:
-                    p = parent[node]
-                    ax.plot([p[1], node[1]], [p[0], node[0]], "green", linewidth=1)
-                    node = p
-            else:
-                # Display "No path found" text in a box
-                ax.text(0.5, 0.5, "No path found", ha="center", va="center", transform=ax.transAxes, fontsize=20, color='red', bbox=dict(facecolor='white', alpha=0.9, edgecolor='red'))
-        else:
-            # Plot the explored nodes in orange
-            for node, p in parent.items():
-                if p is not None:
-                    ax.plot([p[1], node[1]], [p[0], node[0]], "orange", linewidth=1)
-        
-        ax.legend()
-    
-    ani = animation.FuncAnimation(fig, update, frames=len(steps), repeat=False, interval=200)
-    plt.show()
+from animation import animate_pathfinding
 
 if __name__ == "__main__":
     # Load configuration from YAML file
